cb9252761b5c49f3dddc1b70e3707d7d400ace55
Dimitry <dimitrysukalo@gmail.com>
2020-11-02 17:56:59 +0200

    Добавлено дерево папок на локальных дисках пользователя

--- EncryptionApp/Main.Designer.cs
+++ EncryptionApp/Main.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.LogsItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SiteItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileExplorer = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LogsItem,
+            this.SiteItem,
+            this.HelpItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1111, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "TopMenu";
+            // 
+            // LogsItem
+            // 
+            this.LogsItem.Name = "LogsItem";
+            this.LogsItem.Size = new System.Drawing.Size(44, 20);
+            this.LogsItem.Text = "Logs";
+            // 
+            // SiteItem
+            // 
+            this.SiteItem.Name = "SiteItem";
+            this.SiteItem.Size = new System.Drawing.Size(61, 20);
+            this.SiteItem.Text = "Our Site";
+            // 
+            // HelpItem
+            // 
+            this.HelpItem.Name = "HelpItem";
+            this.HelpItem.Size = new System.Drawing.Size(44, 20);
+            this.HelpItem.Text = "Help";
+            // 
+            // FileExplorer
+            // 
+            this.FileExplorer.Location = new System.Drawing.Point(13, 28);
+            this.FileExplorer.Name = "FileExplorer";
+            this.FileExplorer.Size = new System.Drawing.Size(301, 511);
+            this.FileExplorer.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1111, 551);
+            this.Controls.Add(this.FileExplorer);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Main";
             this.Text = "Main";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem LogsItem;
+        private System.Windows.Forms.ToolStripMenuItem SiteItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpItem;
+        private System.Windows.Forms.TreeView FileExplorer;
     }
 }
--- EncryptionApp/Main.cs
+++ EncryptionApp/Main.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace EncryptionApp.UI
@@ -13,6 +9,90 @@ namespace EncryptionApp.UI
         public Main()
         {
             InitializeComponent();
+
+            FileExplorer.BeforeSelect += FileExplorer_BeforeSelect;
+            FileExplorer.BeforeExpand += FileExplorer_BeforeExpand;
+
+            FillDriveNodes();
+        }
+
+        private void FileExplorer_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+        private void FileExplorer_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode node = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillTreeNode(node, dirs[i]);
+                            e.Node.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                if (driveNode != null && !string.IsNullOrWhiteSpace(path))
+                {
+                    string[] dirs = Directory.GetDirectories(path);
+                    foreach (var dir in dirs)
+                    {
+                        TreeNode dirNode = new TreeNode();
+                        dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+
+                        driveNode.Nodes.Add(dirNode);
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void FillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode { Text = drive.Name };
+                    FillTreeNode(driveNode, drive.Name);
+                    FileExplorer.Nodes.Add(driveNode);
+                }
+            }
+            catch (Exception) { }
         }
     }
 }
