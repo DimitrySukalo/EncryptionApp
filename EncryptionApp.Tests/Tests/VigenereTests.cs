c777fa371ac61f10d663c849cb0eeb2a01711da8
Dimitry <dimitrysukalo@gmail.com>
2020-11-12 17:24:57 +0200

    Добвлены тесты для метода шифрования "Виженер"

---
+++ EncryptionApp.Tests/Tests/VigenereTests.cs
@@ -0,0 +1,60 @@
+﻿using EncryptionApp.Models.Models;
+using System;
+using Xunit;
+
+namespace EncryptionApp.Tests.Tests
+{
+    public class VigenereTests
+    {
+        [Fact]
+        private void EncryptMethodReturnsMessage()
+        {
+            //Arrange
+            var vigenereCipher = new VigenereCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = vigenereCipher.Encrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void DecryptMethodReturnsMessage()
+        {
+            //Arrange
+            var vigenereCipher = new VigenereCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = vigenereCipher.Decrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void MessagesAreEqual()
+        {
+            //Arrange
+            var vigenereCipher = new VigenereCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = vigenereCipher.Encrypt(message, key);
+            var decryptedMessage = vigenereCipher.Decrypt(encryptredMessage, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+            Assert.NotNull(decryptedMessage);
+            Assert.Equal(message.ToLower(), decryptedMessage.ToLower());
+        }
+    }
+}
