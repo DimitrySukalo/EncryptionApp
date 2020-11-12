1e30fe3c141d8ef3f4edb6425b11e7dfe76d97fa
Dimitry <dimitrysukalo@gmail.com>
2020-11-12 17:22:38 +0200

    Добавлены тесты для метода шифрования "Цезарь"

---
+++ EncryptionApp.Tests/Tests/CaesarTests.cs
@@ -0,0 +1,60 @@
+﻿using EncryptionApp.Models.Models.Ciphers;
+using System;
+using Xunit;
+
+namespace EncryptionApp.Tests.Tests
+{
+    public class CaesarTests
+    {
+        [Fact]
+        private void EncryptMethodReturnsMessage()
+        {
+            //Arrange
+            var caesarCipher = new CaesarCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = new Random().Next(1, 100);
+
+            //Act
+            var encryptredMessage = caesarCipher.Encrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void DecryptMethodReturnsMessage()
+        {
+            //Arrange
+            var caesarCipher = new CaesarCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = new Random().Next(1, 100);
+
+            //Act
+            var encryptredMessage = caesarCipher.Decrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void MessagesAreEqual()
+        {
+            //Arrange
+            var caesarCipher = new CaesarCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = new Random().Next(1, 100);
+
+            //Act
+            var encryptredMessage = caesarCipher.Encrypt(message, key);
+            var decryptedMessage = caesarCipher.Decrypt(encryptredMessage, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+            Assert.NotNull(decryptedMessage);
+            Assert.Equal(message.ToLower(), decryptedMessage.ToLower());
+        }
+    }
+}
