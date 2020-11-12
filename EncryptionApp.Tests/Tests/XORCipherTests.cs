4de6be344c64b53b5077787423eaf197f2ffe96e
Dimitry <dimitrysukalo@gmail.com>
2020-11-12 17:17:02 +0200

    Добавлены тесты для метода XOR

---
+++ EncryptionApp.Tests/Tests/XORCipherTests.cs
@@ -0,0 +1,60 @@
+﻿using EncryptionApp.Models.Models;
+using System;
+using Xunit;
+
+namespace EncryptionApp.Tests.Tests
+{
+    public class XORCipherTests
+    {
+        [Fact]
+        private void EncryptMethodReturnsMessage()
+        {
+            //Arrange
+            var xorCipher = new XORCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = xorCipher.Encrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void DecryptMethodReturnsMessage()
+        {
+            //Arrange
+            var xorCipher = new XORCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = xorCipher.Decrypt(message, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+        }
+
+        [Fact]
+        private void MessagesAreEqual()
+        {
+            //Arrange
+            var xorCipher = new XORCipher();
+            var message = Guid.NewGuid().ToString();
+
+            var key = Guid.NewGuid().ToString();
+
+            //Act
+            var encryptredMessage = xorCipher.Encrypt(message, key);
+            var decryptedMessage = xorCipher.Decrypt(encryptredMessage, key);
+
+            //Assert
+            Assert.NotNull(encryptredMessage);
+            Assert.NotNull(decryptedMessage);
+            Assert.Equal(message.ToLower(), decryptedMessage.ToLower());
+        }
+    }
+}
--- EncryptionApp/Main.Designer.cs
+++ EncryptionApp/Main.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.ToolBar = new System.Windows.Forms.MenuStrip();
             this.LogsItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.SiteItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.HelpItem = new System.Windows.Forms.ToolStripMenuItem();
             this.FileExplorer = new System.Windows.Forms.TreeView();
             this.LogInfo = new System.Windows.Forms.ListBox();
             this.ActionTitle = new System.Windows.Forms.Label();
@@ -73,12 +71,10 @@
             this.ToolBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.ToolBar.Dock = System.Windows.Forms.DockStyle.None;
             this.ToolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.LogsItem,
-            this.SiteItem,
-            this.HelpItem});
+            this.LogsItem});
             this.ToolBar.Location = new System.Drawing.Point(0, 0);
             this.ToolBar.Name = "ToolBar";
-            this.ToolBar.Size = new System.Drawing.Size(157, 24);
+            this.ToolBar.Size = new System.Drawing.Size(52, 24);
             this.ToolBar.TabIndex = 1;
             this.ToolBar.Text = "TopMenu";
             // 
@@ -88,18 +84,6 @@
             this.LogsItem.Size = new System.Drawing.Size(44, 20);
             this.LogsItem.Text = "Logs";
             this.LogsItem.Click += new System.EventHandler(this.LogButton_Click);
-            // 
-            // SiteItem
-            // 
-            this.SiteItem.Name = "SiteItem";
-            this.SiteItem.Size = new System.Drawing.Size(61, 20);
-            this.SiteItem.Text = "Our Site";
-            // 
-            // HelpItem
-            // 
-            this.HelpItem.Name = "HelpItem";
-            this.HelpItem.Size = new System.Drawing.Size(44, 20);
-            this.HelpItem.Text = "Help";
             // 
             // FileExplorer
             // 
@@ -457,8 +441,6 @@
         #endregion
         private System.Windows.Forms.MenuStrip ToolBar;
         private System.Windows.Forms.ToolStripMenuItem LogsItem;
-        private System.Windows.Forms.ToolStripMenuItem SiteItem;
-        private System.Windows.Forms.ToolStripMenuItem HelpItem;
         private System.Windows.Forms.TreeView FileExplorer;
         private System.Windows.Forms.ListBox LogInfo;
         private System.Windows.Forms.Label ActionTitle;
